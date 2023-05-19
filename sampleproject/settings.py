1fa7e2229c5382147bb732ac4fb218616c6eb573
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-05-19 19:16:54 +0700

    feat(django): setting up django for postgres

--- sampleproject/settings.py
+++ sampleproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 if 'SECRET_KEY' in os.environ:
     SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['db', '198.211.99.20', 'localhost', '127.0.0.1']
 DEBUG = True
 
 # Application definition
@@ -81,12 +81,38 @@ WSGI_APPLICATION = "sampleproject.wsgi.application"
 
 MAX_CONN_AGE = 600
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+
+POSTGRES_READY = (
+    POSTGRES_DB is not None
+    and POSTGRES_PASSWORD is not None
+    and POSTGRES_USER is not None
+    and POSTGRES_HOST is not None
+    and POSTGRES_PORT is not None
+)
+
+if POSTGRES_READY:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": POSTGRES_DB,
+            "USER": POSTGRES_USER,
+            "PASSWORD": POSTGRES_PASSWORD,
+            "HOST": POSTGRES_HOST,
+            "PORT": POSTGRES_PORT,
+        }
     }
-}
+else:
+    DATABASES = {
+		"default": {
+			"ENGINE": "django.db.backends.sqlite3",
+			"NAME": os.path.join(BASE_DIR, "/sqlite/db.sqlite3")
+		}
+	}
 
 if "DATABASE_URL" in os.environ:
     # Configure Django for DATABASE_URL environment variable.
